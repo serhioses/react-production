366f57d83ca6438bdc985b092708eda63173bbfe
serhioses <serega.mirosh@gmail.com>
2024-05-15 23:33:33 +0300

    Disable color mode and language switch from Navbar in storybook (#7)

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -9,7 +9,6 @@ const meta = {
   component: Navbar,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  parameters: { actions: { argTypesRegex: '^on.*' } },
 } satisfies Meta<typeof Navbar>;
 
 export default meta;
