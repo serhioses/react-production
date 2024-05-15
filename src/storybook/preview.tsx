c78ee4b34e824468314696c6d4d55f30ca29f14c
serhioses <serega.mirosh@gmail.com>
2024-05-15 23:31:18 +0300

    Disable color mode and language switch from Navbar in storybook

--- src/storybook/preview.tsx
+++ src/storybook/preview.tsx
@@ -1,12 +1,26 @@
 import { Suspense, useEffect } from 'react';
-import { I18nextProvider } from 'react-i18next';
+import { I18nContext } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 
-import { ChakraBaseProvider, useColorMode } from '@chakra-ui/react';
+import { ChakraBaseProvider, ColorModeContext, useColorMode } from '@chakra-ui/react';
 import type { Preview } from '@storybook/react';
+import { Callback, TFunction } from 'i18next';
 
 import i18n from 'shared/configs/i18n/i18n';
 import { theme } from 'shared/theme/theme';
+
+const originalChangeLanguage = i18n.changeLanguage;
+
+// Mock the changeLanguage method
+const mockChangeLanguage = (
+  _?: string,
+  callback?: Callback,
+): Promise<TFunction<'translation', undefined>> => {
+  return originalChangeLanguage.call(i18n, 'en', callback);
+};
+
+// Override i18n's changeLanguage method with the mocked function
+i18n.changeLanguage = mockChangeLanguage;
 
 interface ColorModeProps {
   colorMode: 'light' | 'dark';
@@ -34,16 +48,30 @@ const preview: Preview = {
   },
   decorators: [
     (Story, context) => {
+      context;
+
       return (
         <ChakraBaseProvider theme={theme}>
           <ColorMode colorMode={context.globals.colorMode}>
-            <MemoryRouter>
-              <Suspense fallback={<p>Loading translations...</p>}>
-                <I18nextProvider i18n={i18n}>
-                  <Story />
-                </I18nextProvider>
-              </Suspense>
-            </MemoryRouter>
+            <ColorModeContext.Provider
+              value={{
+                colorMode: context.globals.colorMode,
+                toggleColorMode: () => null,
+                setColorMode: (value) => value,
+              }}
+            >
+              <MemoryRouter>
+                <Suspense fallback={<p>Loading translations...</p>}>
+                  <I18nContext.Provider
+                    value={{
+                      i18n,
+                    }}
+                  >
+                    <Story />
+                  </I18nContext.Provider>
+                </Suspense>
+              </MemoryRouter>
+            </ColorModeContext.Provider>
           </ColorMode>
         </ChakraBaseProvider>
       );
