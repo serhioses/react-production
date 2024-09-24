1b993068bdd46c345bd746f19c29139ecc4aeeee
sergii-myroshnyk-windmill <sergii.myroshnyk@windmill.digital>
2024-09-24 17:08:25 +0300

    Type chakra base theme

--- src/shared/theme/theme.ts
+++ src/shared/theme/theme.ts
@@ -1,38 +1,58 @@
-import { extendBaseTheme, theme as chakraTheme, withDefaultColorScheme } from '@chakra-ui/react';
+import {
+  extendBaseTheme,
+  theme as chakraTheme,
+  withDefaultColorScheme,
+  baseTheme,
+  useTheme,
+} from '@chakra-ui/react';
 
 const { Alert, Button, Link, Spinner, Modal, Input, FormLabel, FormError, Form } =
   chakraTheme.components;
 
-export const theme = extendBaseTheme(
-  {
-    colors: {
-      primary: chakraTheme.colors.blue,
-    },
-    styles: {
-      global: {
-        html: {
-          height: '100%',
-        },
-        body: {
-          height: '100%',
-          fontFamily: 'Arial, sans-serif',
-        },
-        '#root': {
-          height: '100%',
-        },
+type BaseTheme = typeof baseTheme;
+
+type Colors = BaseTheme['colors'] & {
+  primary: BaseTheme['colors']['blue'];
+};
+
+type ThemeConfig = Omit<BaseTheme, 'colors'> & {
+  colors: Partial<Colors>;
+};
+
+const themeConfig: Partial<ThemeConfig> = {
+  colors: {
+    primary: chakraTheme.colors.blue,
+  },
+  styles: {
+    global: {
+      html: {
+        height: '100%',
+      },
+      body: {
+        height: '100%',
+        fontFamily: 'Arial, sans-serif',
+      },
+      '#root': {
+        height: '100%',
       },
     },
-    components: {
-      Alert,
-      Button,
-      Link,
-      Spinner,
-      Modal,
-      Input,
-      FormError,
-      FormLabel,
-      Form,
-    },
   },
+  components: {
+    Alert,
+    Button,
+    Link,
+    Spinner,
+    Modal,
+    Input,
+    FormError,
+    FormLabel,
+    Form,
+  },
+};
+
+export const theme = extendBaseTheme(
+  themeConfig,
   withDefaultColorScheme({ colorScheme: 'primary' }),
 );
+
+export const useAppTheme = useTheme<Required<typeof themeConfig>>;
